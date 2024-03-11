fc63a62011f02ea1b6ced79e2940a9fb845bdff5
Xavi Umbert Medina <xaviss130@gmail.com>
2024-03-11 21:46:34 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oficial.</title>
+</head>
+<body>
+    <p>Holas</p>
+</body>
+</html>
